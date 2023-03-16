eaa86a0d3f33f913900de96c390a2080a23ea35e
Red10ne <redboukri@gmail.com>
2023-03-16 03:56:14 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-	char str[] ="Programming is like building a multilingual puzzle";
-	puts(str);
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str); /*output the string*/
 	return (0);
 }
