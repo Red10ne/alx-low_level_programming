3ea3c61bcfce05f2b591cd95fc4c3945833e977f
Red10ne <redboukri@gmail.com>
2023-03-16 05:31:08 -0700

    Write a C program that prints exactly and that piece of art is useful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int mmain(void)
+{
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+	retuen (1);
+}
