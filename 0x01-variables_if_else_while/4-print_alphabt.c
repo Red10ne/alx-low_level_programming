1a83a6e78455f6f1a74e185b5acb0825cbc1b241
Red10ne <redboukri@gmail.com>
2023-03-17 13:06:18 -0700

    prints the alphabet in lowercase without 'e' and 'q'.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * main - prints the alphabet in lowercase except 'q' and 'e'
+ * main - Prints the alphabet without q and e.
  *
  * Return: Always 0 (Success)
  */
@@ -13,11 +13,10 @@ int main(void)
 
 	for (i = 97; i < 123; i++)
 	{
-	     if ( i != 101 && i != 113)
-	     {
-	      putchar(i);
-	     }
-	     
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
