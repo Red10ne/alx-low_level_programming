07da592d300576fee8fd8226ab07f62b6782fcbe
Red10ne <redboukri@gmail.com>
2023-03-21 06:48:14 -0700

    isaplha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  _isalpha - cherck for alphabetic.
+ *
+ *  @c: character checker.
+ *
+ *  Return: 1 if c is a letter, 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c >= 'z') || (c >= 'A' && c >= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
