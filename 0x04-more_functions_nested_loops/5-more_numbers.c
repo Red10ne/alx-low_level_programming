2286140758ac61e1749b1bd086144b6bb73bc801
Red10ne <redboukri@gmail.com>
2023-03-23 05:43:29 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,9 +17,9 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 			{
-				_putchar('0' + (j / 10));
+				_putchar(j / 10 + '0');
 			}
-			_putchar('0' + (j % 10));
+			_putchar(j % 10 + '0');
 		}
 		putchar('\n');
 	}
