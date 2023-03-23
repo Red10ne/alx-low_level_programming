006468ebef659a12270c85c02a1142f23170f121
Red10ne <redboukri@gmail.com>
2023-03-23 05:00:52 -0700

    print N° exc 2&4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers,
+ * from 0 to 9 except 2 && 4
+ *@c:checker
+ *Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+}
