bafe519f94bc37c66e64368bc4a048cd6b089cca
Red10ne <redboukri@gmail.com>
2023-03-27 09:18:17 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ * 
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (len >= 0)
+	{
+		if (str[len] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		if (len % 2 == 0)
+		
+			putchar(str[len]);
+		len++;
+	}
+}
+
