fd190ec2c11f0231c6afc726dbf19d54d96c6577
Red10ne <redboukri@gmail.com>
2023-03-27 09:21:36 -0700

    betty

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 
 /**
  * puts2 - function that prints every other character of a string
- * 
+ * @str:input string
  * Return: 0
  */
 
@@ -18,9 +18,8 @@ void puts2(char *str)
 			break;
 		}
 		if (len % 2 == 0)
-		
+
 			putchar(str[len]);
 		len++;
 	}
 }
-
