2523e9a5ff68fdf237a9fdc17e45a144fcea11ac
Red10ne <redboukri@gmail.com>
2023-04-06 05:45:44 -0700

    fix code

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,15 @@
 
 /**
  * main - program that prints it's name
+ * @argc: number of arg
+ * @argv: arguments
  * Return: 0
  */
 
 int main(int argc, char **argv)
 {
 	int i;
+
 	if (argc <= 1)
 	{
 		for (i  = 0; i < argc; i++)
@@ -18,4 +21,3 @@ int main(int argc, char **argv)
 	}
 	return (0);
 }
-	
