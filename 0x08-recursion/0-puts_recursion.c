7cf58a0e996c868bd6a35d86206b0e61c605764d
Red10ne <redboukri@gmail.com>
2023-04-06 00:10:48 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string
+ *
+ * @s: string
+ * Return: 1
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(*s);
+}
