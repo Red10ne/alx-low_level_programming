82d8fbc9aeae125d13bba24bb99b7a234091ae02
Red10ne <redboukri@gmail.com>
2023-04-11 08:33:24 -0700

    fix code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,9 +17,12 @@ char *str_concat(char *s1, char *s2)
 	int total_len;
 	char *result;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
 	len1 = strlen(s1);
