ca01794da8391a8d9da6002c5951bcdd8072533a
Red10ne <redboukri@gmail.com>
2023-04-28 04:59:59 -0700

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,20 +10,27 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	size_t nchar;
+	char *dup;
+	int len;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (*head);
+	}
+	for (len = 0; str[len];)
+		len++;
 
-	for (nchar = 0; str[nchar]; nchar++)
-		;
-
-	new->len = nchar;
+	new->str = dup;
+	new->len = len;
 	new->next = *head;
+
 	*head = new;
 
-	return (*head);
+	return (new);
 }
