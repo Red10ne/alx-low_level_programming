ab3e29aa947be448f86f50422976fa3513d458e5
Red10ne <redboukri@gmail.com>
2023-04-28 03:52:20 -0700

    fix betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_lists - prints all th elements of a list_t list
+ * print_list - prints all the elements of a list_t list.
  * @h: singly linked list.
  * Return: number of  elem. in the list.
  */
