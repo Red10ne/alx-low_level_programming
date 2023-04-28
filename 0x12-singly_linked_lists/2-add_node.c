ee670c5f3a9ae08a2a0ba8a64ca1fb44d2faab48
Red10ne <redboukri@gmail.com>
2023-04-28 04:38:01 -0700

    fix code

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,4 @@
-#include <lists.h>
+#include "lists.h"
 
 /**
  * add_node - adds a new node at the beginning
@@ -8,15 +8,15 @@
  * Return: address of the head
  */
 
-lsit_t *add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	size_t nchar;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-		return(NULL);
-	new ->str = strdup(str);
+		return (NULL);
+	new->str = strdup(str);
 
 	for (nchar = 0; str[nchar]; nchar++)
 		;
