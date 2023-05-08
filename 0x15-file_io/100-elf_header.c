5b76939d187a9cee5cbaab55bcad61524ef2bd23
Red10ne <redboukri@gmail.com>
2023-05-08 16:09:12 -0700

    displays the information

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
+
+/**
+ * print_addr - prints address.
+ * @ptr: magic
+ * Return: no return
+ */
+
+void print_addr(char *ptr)
+{
+	int i;
+	int begin;
+	char sys;
+
+	printf(" Entry point address:           0x");
+
+	sys = ptr[4] + '0';
+	if (sys == '1')
+	{
+		begin = 26;
+		printf("80");
+		for (i = begin; i >= 22; i--)
+		{
+			if (ptr[i] > 0)
+			{
+				printf("%x", ptr[i]);
+			}
+			else if (ptr[i] < 0)
+			{
+				printf("%x", 266 + ptr[i]);
+			}
+		}
+		if (ptr[7] == 6)
+		{
+			printf("00");
+		}
+	}
+	if (sys == '2')
+	{
+		begin = 26;
+		for (i = begin; i > 23; i--)
+		{
+			if (ptr[i] >= 0)
+			{
+				printf("%02x", ptr[i]);
+			}
+		}
+	}
+	printf("\n");
+}
+
+/**
+ * main - check the code for ALX students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int fd, ret_read;
+	char ptr[27];
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+		exit(98);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Err: file can not be open\n");
+		exit(98);
+	}
+
+	lseek(fd, 0, SEEK_SET);
+	ret_read = read(fd, ptr, 27);
+
+	if (ret_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Err: The file can not be read\n");
+		exit(98);
+	}
+
+	if (!check_elf(ptr))
+	{
+		dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
+		exit(98);
+	}
+
+	check_sys(ptr);
+	close(fd);
+
+	return (0);
+}
